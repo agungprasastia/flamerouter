d0295b435b6603f9bee4c3142baa7d1fe065fdf8
agungprasastia <154116686+agungprasastia@users.noreply.github.com>
2026-08-27 07:59:11 +0000

    refactor: extract useOAuthCallback hook from CallbackContent

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,3 +1,5 @@
 // Shared Hooks - Export all
 export { useTheme } from "./useTheme";
 export { useModelCaps } from "./useModelCaps";
+export { useOAuthCallback } from "./useOAuthCallback";
+export type { OAuthCallbackStatus } from "./useOAuthCallback";
